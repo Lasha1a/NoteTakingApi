724712609e28338db05e83547d0b2906e51be6d6
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-02-13 12:28:17 +0400

    fixed registerUser not showing up in github

--- NoteTaking.Api/Features/Auth/RegisterUser.cs
+++ NoteTaking.Api/Features/Auth/RegisterUser.cs
@@ -1,6 +1,51 @@
-﻿namespace NoteTaking.Api.Features.Auth
+﻿using Microsoft.AspNetCore.Identity;
+using NoteTaking.Api.Common.models;
+using Microsoft.EntityFrameworkCore;
+using NoteTaking.Api.Infrastructure.Data;
+
+namespace NoteTaking.Api.Features.Auth;
+
+
+
+public static class RegisterUser
 {
-    public class RegisterUser
+    public record Request(string Email, string Password);
+
+    public record Response(Guid Id, string Email);
+
+    public static void Map(IEndpointRouteBuilder app) =>
+
+        app.MapGet("/auth/register", Handle)
+            .WithName(nameof(RegisterUser))
+            .WithOpenApi();
+
+    static async Task<IResult> Handle(
+        Request request,
+        AppDbContext db,
+        IPasswordHasher<User> passwordHasher,
+        CancellationToken ct)
     {
+        var exists = await db.Users
+            .AnyAsync(u => u.Email == request.Email, ct);
+
+        if (exists )
+            return Results.BadRequest("Email alrdy exists");
+
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            Email = request.Email,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        user.PasswordHash = passwordHasher.HashPassword(user, request.Password);
+
+        db.Users.Add(user);
+        await db.SaveChangesAsync(ct);
+
+        return Results.Ok(new Response(user.Id, user.Email));
     }
+
 }
+
+
