40f87bb5d350c8d2440d6193d1940f5c92cf2d84
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-02-14 21:48:21 +0400

    created UpdateNote endpoint and mapped it into program.cs

---
+++ NoteTaking.Api/Features/Notes/UpdateNote.cs
@@ -0,0 +1,12 @@
+﻿namespace NoteTaking.Api.Features.Notes;
+
+public class UpdateNote
+{
+    public record class Request(
+        string Title,
+        string? Content
+    );
+
+    public static void Map(IEndpointRouteBuilder app) =>
+        app.MapPut("/notes/{id;Guid}", Handle)
+}
