47bde32bf375c5fc2ad8179ee304676d0701145a
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-02-15 21:58:12 +0400

    added TokenService class and logic

---
+++ NoteTaking.Api/Features/Auth/TokenService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using NoteTaking.Api.Common.models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace NoteTaking.Api.Features.Auth;
+
+public class TokenService
+{
+    private readonly IConfiguration _config;
+
+    public TokenService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public string GenerateAccessToken(User user) // generates a JWT access token for the given user
+    {
+        var jwt = _config.GetSection("Jwt"); // retrieves the JWT configuration section from the application's configuration settings
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()), // adds a claim for the user's unique identifier (ID)
+            new Claim(ClaimTypes.Email, user.Email)
+        };
+
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(jwt["Key"]!)
+        );
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken( // creates a new JWT token with the specified issuer, audience, claims, expiration time, and signing credentials
+            issuer: jwt["Issuer"],
+            audience: jwt["Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(15),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    public RefreshToken GenerateRefreshToken(Guid userId) // generates a new refresh token for the specified user ID
+    {
+        var bytes = RandomNumberGenerator.GetBytes(64);
+
+        return new RefreshToken
+        {
+            Id = Guid.NewGuid(),
+            UserId = userId,
+            Token = Convert.ToBase64String(bytes),
+            ExpiresAt = DateTime.UtcNow.AddDays(7),
+            CreatedAt = DateTime.UtcNow,
+            IsRevoked = false
+        };
+    }
+}
